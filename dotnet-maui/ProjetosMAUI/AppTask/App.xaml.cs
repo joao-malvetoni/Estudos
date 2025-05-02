92b25d7ffb70a80ec9b616b656466e2052f3b4d9
joao-malvetoni <joao.malvetoni@outlook.com>
2025-05-02 17:02:23 -0300

    Início do projeto de tasks

---
+++ dotnet-maui/ProjetosMAUI/AppTask/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using AppTask.Views;
+using Microsoft.Maui.Platform;
+
+namespace AppTask
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			CustomHandler();
+			InitializeComponent();
+		}
+
+		private void CustomHandler()
+		{
+			Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoBorder", (handler, view) => {
+#if ANDROID
+				// ANDROID
+				handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+
+#elif IOS || MACCATALYST
+				// iOS || MACCATALYST
+				handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+				// Windows
+				handler.PlatformView.BorderThickness = new Thickness(0).ToPlatform();
+#endif
+			});
+		}
+
+		protected override Window CreateWindow(IActivationState? activationState)
+		{
+			return new Window(new NavigationPage(new StartPage()));
+		}
+	}
+}
---
+++ dotnet-maui/ProjetosMAUI/AppTask/Views/AddEditTaskPage.xaml.cs
@@ -0,0 +1,24 @@
+namespace AppTask.Views;
+
+public partial class AddEditTaskPage : ContentPage
+{
+	public AddEditTaskPage()
+	{
+		InitializeComponent();
+	}
+
+	private void CloseModal(object sender, EventArgs e)
+	{
+		Navigation.PopModalAsync();
+    }
+
+	private void SaveData(object sender, EventArgs e)
+	{
+		Navigation.PopModalAsync();
+	}
+
+	private async void AddStep(object sender, EventArgs e)
+	{
+		var steName = await DisplayPromptAsync("Etapa (subtarefa)", "Digite o nome da etapa (subtarefa)", "Adicionar", "Cancelar");
+	}
+}
